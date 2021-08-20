535b4640618451b69d6d4f5c61b98450c5b37ed9
Jason S. Robinson <json.robinson@gmail.com>
2021-08-20 10:27:57 -0500

    fix: SLY listp, 'ccl' error.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,10 +15,19 @@
   :ensure t
   ;; Invoke with `M--M-x sly'
   :init (setq sly-lisp-implementations
-	      '((ccl "ccl")
+	      '((ccl ("ccl"))
 		(cmucl ("/opt/cmucl-20c/bin/lisp" "-quiet"))))
   :config (setq inferior-lisp-program "ccl"))
 
 (use-package material-theme
   :ensure t
   :config (load-theme 'material-light t))
+
+;;; Alternate themes I have enjoyed and will switch to on occasion:
+;; - DARK -
+;; NORD - https://github.com/arcticicestudio/nord-emacs
+;; TRON
+;; - LIGHT -
+;; SOLARIZED
+
+
