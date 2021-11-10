1129ef2095afc9745e3a6e73448094695b3ac007
Avery Yuons <stillplaying@makebelieve.com>
2021-11-10 11:42:09 -0500

    Using NORD theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -226,13 +226,14 @@
 
 
 
-;;; Alternate themes I have enjoyed and will switch to on occasion:
+;; Alternate themes I have enjoyed and will switch to on occasion:
 ;; - DARK -
 ;; NORD - https://github.com/arcticicestudio/nord-emacs
-;(use-package nord-theme
-;  :ensure nil
-;  :config (load-theme 'nord t)
-;  (setq nord-region-highlight "snowstorm"))
+
+;; (use-package nord-theme
+;;  :ensure t
+;;  :config (load-theme 'nord t)
+;;  (setq nord-region-highlight "snowstorm"))
 
 ;; TRON
 ;(use-package tron-legacy-theme
@@ -249,8 +250,28 @@
 ;  (setq solarized-high-contrast-mode-line t))
 
 ;; - LIGHT -
-;; FLATUI
 
+;; NORD
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-nord-light t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; FLATUI
+;; FLUCUI
+;; (use-package flucui-themes
+;;   :ensure t
+;;   :config (load-theme 'flucui-light t))
 ;; SOLARIZED
 
 ;; MATERIAL-LIGHT
