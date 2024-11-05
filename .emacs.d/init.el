b95e06bdea818702c662abe607e605ecde5106a7
Jason Robinson <jason.robinson>
2024-11-05 18:56:17 -0500

    feat(ui): hide modeline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,6 +26,9 @@
 (setq inhibit-startup-screen t)
 ;; Don't use messages that you don't read
 (setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message
+      "Welcome, Jason! What are we creating today?")
 
 ;; Graphical User interface settings
 (if (fboundp 'menu-bar-mode) (menu-bar-mode 0))
@@ -52,6 +55,51 @@
 (display-time)
 ;; Save backups to a central directory
 (setq backup-directory-alist `(("." . "~/.emacs.saves")))
+
+;; See http://bzg.fr/emacs-hide-mode-line.html
+(defvar-local hidden-mode-line-mode nil)
+(defvar-local hide-mode-line nil)
+
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global nil
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+;; Activate hidden-mode-line-mode
+(hidden-mode-line-mode 1)
+
+;; If you want to hide the mode-line in all new buffers
+(add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+;; Command to toggle the display of the mode-line as a header
+(defvar-local header-line-format nil)
+(defun mode-line-in-header ()
+  (interactive)
+  (if (not header-line-format)
+      (setq header-line-format mode-line-format
+    	    mode-line-format nil)
+    (setq mode-line-format header-line-format
+    	  header-line-format nil))
+  (set-window-buffer nil (current-buffer)))
+(global-set-key (kbd "<f1>") 'mode-line-in-header)
+
 
 ;;; --- END VISUALS
 
